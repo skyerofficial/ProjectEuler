2d1bc6b56e6a1e8937df9be1b1477e51f7b86707
gunavina <gunavinamehta@gmail.com>
2016-10-29 15:08:26 +0530

    Update problem1.c

--- C/problem1.c
+++ C/problem1.c
@@ -1,13 +1,3 @@
-/*
-Project Euler #4: Largest palindrome product
-
-A palindromic number reads the same both ways. The smallest 6 digit palindrome made from the product of two 3-digit numbers is 101101 = 143 X 707.
-. 
-Find the largest palindrome made from the product of two 3-digit numbers which is less than N.
-
-Input Format:
-First line contains T that denotes the number of test cases. This is followed by T lines, each containing an integer, N.
-*/
 #include <math.h>
 #include <stdio.h>
 #include <string.h>
