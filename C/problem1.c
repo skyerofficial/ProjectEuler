0e7834ecaf8451e89dad44cea7dbb2d32a0a988a
gunavina <gunavinamehta@gmail.com>
2016-10-31 00:30:22 +0530

    Update problem1.c

--- C/problem1.c
+++ C/problem1.c
@@ -1,3 +1,10 @@
+/*
+Project Euler #1: Multiples of 3 and 5
+
+If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3,5,6 and 9. The sum of these multiples is 23.
+
+First line contains T that denotes the number of test cases. This is followed by T lines, each containing an integer, .
+*/
 #include <math.h>
 #include <stdio.h>
 #include <string.h>
